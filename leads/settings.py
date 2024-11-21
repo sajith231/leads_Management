a13be02a1ab863218c95cd3671655de00890bf5f
sajith231 <sajiththomas231@gmail.com>
2024-11-21 10:58:04 +0530

    Database chnaged

--- leads/settings.py
+++ leads/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'leads.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'leading_management',
+        'USER': 'postgres',
+        'PASSWORD': 'sajith@2003',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
