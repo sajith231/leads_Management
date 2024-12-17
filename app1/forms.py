6b095a9de17b56b6fad15751e3de54929fb39f12
sajith231 <sajiththomas231@gmail.com>
2024-12-17 14:01:28 +0530

    hai

--- app1/forms.py
+++ app1/forms.py
@@ -186,8 +186,8 @@ class LeadForm(forms.ModelForm):
             'voice_note': forms.FileInput(attrs={'class': 'form-control', 'accept': 'audio/*'}),
             'district': forms.Select(attrs={'class': 'form-select', 'id': 'id_district'}),
             'location': forms.Select(attrs={'class': 'form-select', 'id': 'id_location'}),
-            'district': forms.Select(attrs={'class': 'form-select', 'id': 'id_district'}),
-            'area': forms.Select(attrs={'class': 'form-select', 'id': 'id_area'}),
+            'district': forms.TextInput(attrs={'class': 'form-select', 'id': 'id_district','readonly': True}),
+            'area': forms.TextInput(attrs={'class': 'form-control', 'id': 'id_area', 'readonly': True}),
 
 
             
