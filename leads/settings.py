5bb1f4df5bb07beb65ea52dfc496f09bc491f389
sajith231 <sajiththomas231@gmail.com>
2025-02-27 18:29:23 +0530

    complited

--- leads/settings.py
+++ leads/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'leading_management',
         'USER': 'postgres',
-        'PASSWORD': 'sajith@2003',
+        'PASSWORD': 'info@imc',
         'HOST': 'localhost',
         'PORT': '5432',
     }
