789967645011506e0dd7d612a6e87f6c57d92805
sajith231 <sajiththomas231@gmail.com>
2025-03-21 10:01:19 +0530

    lead sets correcly

--- app1/templatetags/custom_filters.py
+++ app1/templatetags/custom_filters.py
@@ -8,7 +8,9 @@ def get_item(dictionary, key):
     Template filter to get an item from a dictionary using a key.
     Usage: {{ my_dict|get_item:key }}
     """
-    return dictionary.get(key)
+    if isinstance(dictionary, dict):
+        return dictionary.get(key, "")
+    return ""
 
 @register.filter
 def sort_by_latest(cv_list):
