014e811ee6ea071dfdffa6ae3734178a9b531cff
sajith231 <sajiththomas231@gmail.com>
2025-06-02 18:05:08 +0530

    early request

--- leads/settings.py
+++ leads/settings.py
@@ -118,7 +118,7 @@ CKEDITOR_CONFIGS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'leading_managemen',
+        'NAME': 'leading_management',
         'USER': 'postgres',
         'PASSWORD': 'info@imc',
         'HOST': 'localhost',
