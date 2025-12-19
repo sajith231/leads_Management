ac9b54a6261a660b178e9f70f1ecd58372a47c74
Amal <amal06429@gmail.com>
2025-12-19 10:26:04 +0530

    checkout

--- app5/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class App5Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app5'
--- software_master/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.software_table, name='software_table'),
-    path('add/', views.add_software, name='add_software'),
-    path('edit/<int:id>/', views.edit_software, name='edit_software'),
-    path('delete/<int:id>/', views.delete_software, name='delete_software'),
-]
