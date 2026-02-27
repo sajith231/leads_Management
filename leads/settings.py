77027c0e1851f1df2d6812bd03f50c8ed819eb8a
sajith231 <sajiththomas231@gmail.com>
2026-02-27 10:11:34 +0530

    feeder api

--- leads/settings.py
+++ leads/settings.py
@@ -171,8 +171,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'leading_management',
         'USER': 'postgres',
-        'PASSWORD':'12345',
-        'HOST': 'localhost',
+        'PASSWORD':'info@imc',
+        'HOST': '88.222.212.14',
         'PORT': '5432',
     }
 }
